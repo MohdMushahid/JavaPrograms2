31c2d5ef0b54c5ed5412827e4640e4e9f78c7cd1
Promode <pramoddutta_96@yahoo.com>
2025-06-24 07:47:47 +0530

    24th June

--- src/ex_14_Strings/InterviewCoding.java
+++ src/ex_14_Strings/InterviewCoding.java
@@ -12,7 +12,34 @@ public class InterviewCoding {
         // G
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String input = scanner.next();
+//        String newString_reversed = reverseStringSB(input);
+        String newString_reversed = reverseString(input);
+
+        if (newString_reversed.equalsIgnoreCase(input)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+       // madam.chatAt(1) -> a
+
+    }
 
 
+    static String reverseString(String userInput){
+        String reversed = "";
+        for (int i = userInput.length()-1; i >= 0 ; i--) {
+            reversed = reversed+ userInput.charAt(i);
+        }
+        return reversed;
+    }
+
+
+
+
+    static String reverseStringSB(String userInput) {
+        StringBuilder stringBuilder = new StringBuilder(userInput);
+        return stringBuilder.reverse().toString();
     }
 }
